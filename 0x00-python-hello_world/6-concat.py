8f99a964086a0b584b931f03833612692374f4e8
mhiskall282 <johnokyere282@icloud.com>
2024-03-17 19:55:03 +0000

    alx

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
-str1 ="Holberton"
-str2 ="School"
-str1 = str1 + " " + str2
-print(f'Welcome to {str1}!')
+#!/bin/python3
+str1 = "Holberton"
+str2 = "School"
+st1 = str1 + " " + str2
+print(f'Welcome to {st1}!')
+

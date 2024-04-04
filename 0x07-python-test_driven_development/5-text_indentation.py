289525a65d47c034a09499326e81f25916dd3227
mhiskall282 <johnokyere282@icloud.com>
2024-04-04 15:09:43 +0000

    a

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Represent text indentation function"""
+
+
+def text_indentation(text):
+    """
+    Define text indentation
+    Args:
+        text: strings to be printed
+
+    Raises:
+        TypeError: if text is not string
+
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
